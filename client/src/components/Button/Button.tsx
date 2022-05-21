f58b1dead25bac21ef9c7aca2744bbe80e9793b7
hamisk <hs.khera@icloud.com>
2022-05-20 17:00:13 -0700

    Refactor home button to button component

---
+++ client/src/components/Button/Button.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Button.scss';
+
+type Props = {
+  onClick?: any;
+  onClickText?: string;
+  label: string;
+};
+
+function Button({ onClick, onClickText, label }: Props) {
+  return (
+    <button
+      className='cta'
+      onClick={() => {
+        onClick(onClickText);
+      }}>
+      {label}
+    </button>
+  );
+}
+
+export default Button;
+
+// data is a special attribute of html
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import './Home.scss';
 import hk_transparent from '../../assets/images/hk_transparent.png';
+import Button from '../../components/Button/Button';
 
 interface Props {
   scroll: (id: string) => void;
@@ -16,19 +17,9 @@ const Home: React.FC<Props> = ({ scroll }) => {
       <h1 className='home__job-title'>Full-Stack Web Developer</h1>
 
       <div className='home__buttons'>
-        <button
-          className='home__cta'
-          onClick={() => {
-            scroll('#portfolio');
-          }}>
-          {/* <div className='home__button-bg'></div> */}
-          View my work
-        </button>
+        <Button onClick={scroll} onClickText='#portfolio' label='View my work' />
         <a href='mailto:h.khera@protonmail.com' className='home__contact'>
-          <button className='home__cta' onClick={() => {}}>
-            {/* <div className='home__button-bg'></div> */}
-            Get in touch
-          </button>
+          <Button label='Get in touch' />
         </a>
       </div>
     </div>
