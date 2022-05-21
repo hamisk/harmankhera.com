482ff2a39bf6c9b24e4239dde183dd7ffaca169a
hamisk <hs.khera@icloud.com>
2022-05-20 21:35:28 -0700

    Add skills page

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type Props = {};
+
+function Footer({}: Props) {
+  return (
+    <div>
+      <a href='https://www.flaticon.com/free-icons/development' title='development icons'>
+        Development icons created by Freepik - Flaticon
+      </a>
+      <a href='https://www.flaticon.com/free-icons/database' title='database icons'>
+        Database icons created by phatplus - Flaticon
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
--- client/src/pages/Skills/Skills.tsx
+++ client/src/pages/Skills/Skills.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
+import web_dev from '../../assets/icons/front-end.png';
+import back_end from '../../assets/icons/database-storage.png';
+import misc from '../../assets/icons/developing.png';
 import './Skills.scss';
 
-type Props = {};
+// type Props = {};
 
-function Skills({}: Props) {
+function Skills() {
   return (
     <div className='skills'>
       <h2 className='skills__title'>Skills</h2>
+      <div className='skills__cards'>
+        <div className='skillcard'>
+          <img src={web_dev} alt='web development' className='skillcard__icon' />
+          <h3 className='skillcard__title'>Front-End</h3>
+          <ul className='skillcard__list'>
+            <li className='skillcard__list-item'>React</li>
+            <li className='skillcard__list-item'>TypeScript</li>
+            <li className='skillcard__list-item'>JavaScript</li>
+            <li className='skillcard__list-item'>Chart.js</li>
+            <li className='skillcard__list-item'>HTML/CSS</li>
+            <li className='skillcard__list-item'>SASS</li>
+            <li className='skillcard__list-item'>Responsive Design</li>
+          </ul>
+        </div>
+        <div className='skillcard'>
+          <img src={back_end} alt='web development' className='skillcard__icon' />
+          <h3 className='skillcard__title'>Back-End</h3>
+          <ul className='skillcard__list'>
+            <li className='skillcard__list-item'>Node.JS</li>
+            <li className='skillcard__list-item'>Express</li>
+            <li className='skillcard__list-item'>MySQL</li>
+            <li className='skillcard__list-item'>RESTful APIs</li>
+          </ul>
+        </div>
+        <div className='skillcard'>
+          <img src={misc} alt='web development' className='skillcard__icon' />
+          <h3 className='skillcard__title'>Misc</h3>
+          <ul className='skillcard__list'>
+            <li className='skillcard__list-item'>GitHub</li>
+            <li className='skillcard__list-item'>JWT</li>
+            <li className='skillcard__list-item'>Jira</li>
+            <li className='skillcard__list-item'>Agile Development</li>
+            <li className='skillcard__list-item'>Heroku</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
