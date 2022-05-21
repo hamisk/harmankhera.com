84eb7f209c92fef9f331c85f81fcc760104650f0
hamisk <hs.khera@icloud.com>
2022-05-20 17:26:42 -0700

    style preview card

--- client/src/components/Preview/Preview.tsx
+++ client/src/components/Preview/Preview.tsx
@@ -1,4 +1,5 @@
-import './Preview.scss'
+import Button from '../Button/Button';
+import './Preview.scss';
 
 interface Props {
   background: string;
@@ -6,29 +7,31 @@ interface Props {
   tech: string;
   projectName: string;
   setModalProjectName: React.Dispatch<React.SetStateAction<string | boolean>>;
-  style: {transitionDelay: string}
+  style: { transitionDelay: string };
   // setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Preview: React.FC<Props> = ({ background, title, tech, projectName, setModalProjectName, style }) => {  
-
+const Preview: React.FC<Props> = ({ background, title, tech, projectName, setModalProjectName, style }) => {
   return (
     <div className='preview__container' style={style}>
-        <img src={background} alt='screenshot' className='preview__image' />
-        <div className='preview__overlay' onClick={()=>{}}>
-            <div className='preview__text'>
-              <h3 className="preview__title">{title}</h3>
-              <p className="preview__tech">{tech}</p>
-            </div>
-              <button className="preview__cta"
-                onClick={() => {
-                  // setShowModal(true)
-                  setModalProjectName(projectName)
-                }}>
-                  Learn More</button>
+      <img src={background} alt='screenshot' className='preview__image' />
+      <div className='preview__overlay'>
+        <div className='preview__text'>
+          <h3 className='preview__title'>{title}</h3>
+          <p className='preview__tech'>{tech}</p>
         </div>
+        {/* <button
+          className='preview__cta'
+          onClick={() => {
+            // setShowModal(true)
+            setModalProjectName(projectName);
+          }}>
+          Learn More
+        </button> */}
+        <Button onClick={setModalProjectName} onClickText={projectName} label='Learn more' />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Preview
+export default Preview;
