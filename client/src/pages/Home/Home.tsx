b9f3e77f57a22001641dab6ddd03f629fa450d40
hamisk <hs.khera@icloud.com>
2022-06-10 17:21:33 -0700

    Animate home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,29 +1,31 @@
 import './Home.scss';
 import Button from '../../components/Button/Button';
 import bg_forest from '../../assets/images/forest-background-crop-compressed.jpeg';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 interface Props {
   scroll: (id: string) => void;
 }
 
 const Home: React.FC<Props> = ({ scroll }) => {
+  const [showClass, setShowClass] = useState('');
   useEffect(() => {
     //preloading image
     const img = new Image();
     img.src = bg_forest;
+    setShowClass('show');
   }, []);
 
   return (
     <div className='home' id='home'>
       <img src={bg_forest} alt='forest background' className='home__bg-image' />
       <div className='home__bg-image-overlay'></div>
-      <h1 className='home__title'>
+      <h1 className={'home__title ' + showClass}>
         Hi, I'm <span className='home__name'>Harman Khera</span>
       </h1>
-      <h1 className='home__job-title'>Full-Stack Web Developer</h1>
+      <h1 className={'home__job-title ' + showClass}>Full-Stack Web Developer</h1>
 
-      <div className='home__buttons'>
+      <div className={'home__buttons ' + showClass}>
         <Button onClick={scroll} onClickText='#portfolio' label='View my work' />
         <a href='mailto:h.khera@protonmail.com' className='home__contact'>
           <Button label='Get in touch' />
